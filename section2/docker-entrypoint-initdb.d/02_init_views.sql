afa4b3b5a4ddcd13670bc5df0fa65d7ad538c763
Jocelyn Tan <jocelynpeixuan@gmail.com>
2022-10-09 11:49:37 +0800

    Captitalized keywords

--- section2/docker-entrypoint-initdb.d/02_init_views.sql
+++ section2/docker-entrypoint-initdb.d/02_init_views.sql
@@ -1,13 +1,13 @@
-create or replace view TransactionHistory as
-select transaction_id as transaction_id,
-       s.salesperson_name as salesperson,
-       c2.customer_name  as customer,
-       m.model_name         as model,
-       market_price   as market_price,
-       price_sold     as price_sold,
+CREATE OR REPLACE VIEW TransactionHistory AS
+SELECT transaction_id AS transaction_id,
+       s.salesperson_name AS salesperson,
+       c2.customer_name  AS customer,
+       m.model_name         AS model,
+       market_price   AS market_price,
+       price_sold     AS price_sold,
        transaction_date
-from Transaction t
-left join car c on c.car_id = t.car_id
-left join customer c2 on c2.customer_id = t.customer_id
-left join salesperson s on s.salesperson_id = t.salesperson_id
-left join model m on c.model_id = m.model_id
+FROM Transaction t
+LEFT JOIN car c ON c.car_id = t.car_id
+LEFT JOIN customer c2 ON c2.customer_id = t.customer_id
+LEFT JOIN salesperson s ON s.salesperson_id = t.salesperson_id
+LEFT JOIN model m ON c.model_id = m.model_id
