d3df41bf702a9e74641abca4d1a1e5e37563468d
jocelyntpx <65552325+jocelyntpx@users.noreply.github.com>
2022-10-09 13:04:33 +0800

    update task

--- section2/tasks.sql
+++ section2/tasks.sql
@@ -7,9 +7,11 @@ LEFT JOIN transaction t ON c.customer_id = t.customer_id
 GROUP BY c.customer_id, c.customer_name;
 
 -- 2. I want to find out the top 3 car manufacturers that customers bought by sales (quantity) and the sales number for it in the current month.
-SELECT m2.name, COUNT(transaction_id) AS sales
+SELECT m2.salesperson_id, m2.salesperson_name, COUNT(transaction_id) AS sales_quality, SUM(price_sold) AS sales_amount
 FROM transaction
 LEFT JOIN car c ON transaction.car_id = c.car_id
 LEFT JOIN model m ON c.model_id = m.model_id
 LEFT JOIN manufacturer m2 ON m.manufacturer_id = m2.manufacturer_id
-GROUP BY m2.name ORDER BY sales LIMIT 3
+GROUP BY m2.salesperson_id, m2.salesperson_name 
+ORDER BY sales DESC 
+LIMIT 3
